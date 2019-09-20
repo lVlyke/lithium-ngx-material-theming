f5b97aafcd821464cc00e0344a05606b667692ee
Mychal Thompson <mychalrthompson@gmail.com>
2019-09-19 22:26:26 -0400

    0.4.0-beta.0: - Added ThemeGenerator.createFromTemplate function for creating themes from custom templates - Exposed ThemeGenerator.createPalette, ThemeGenerator.paletteColor, ThemeGeneratorcontrastColor - Removed ThemeGenerator.createBasic and ThemeGenerator.createRandom functions - Exported SASS variables for creating theme templates

--- src/helpers/theme-generator.ts
+++ src/helpers/theme-generator.ts
@@ -2,131 +2,141 @@ import { BASIC_DARK_THEME_TEMPLATE } from "./templates/basic-dark-theme";
 import { BASIC_LIGHT_THEME_TEMPLATE } from "./templates/basic-light-theme";
 import { ThemeLoader } from "./theme-loader";
 
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
 export type PaletteOffset = (keyof ThemeGenerator.Palette) & number;
+
+/**
+ * @param name The name that will be used to reference the new theme.
+ * @param templateData The CSS theme template that will be used to create the final theme data.
+ * @param primaryPalette The primary color palette.
+ * @param accentPalette The accent color palette.
+ * @param warnPalette The warn color palette.
+ */
+export interface TemplateThemeCreationOptions {
+    name: string;
+    templateData: string;
+    primaryPalette: ThemeGenerator.Palette;
+    accentPalette: ThemeGenerator.Palette;
+    warnPalette: ThemeGenerator.Palette;
+    templateOptions?: Partial<ThemeTemplateOptions>;
+}
+
+export interface ThemeTemplateOptions {
+    nameMatcher: string | RegExp;
+    primaryColorMatcher: string | RegExp;
+    accentColorMatcher: string | RegExp;
+    warnColorMatcher: string | RegExp;
+    primaryContrastColorMatcher: string | RegExp;
+    accentContrastColorMatcher: string | RegExp;
+    warnContrastColorMatcher: string | RegExp;
+}
+
+export namespace ThemeTemplateOptions {
+
+    export const defaultValues: Readonly<ThemeTemplateOptions> = {
+        nameMatcher: /%theme-name%/g,
+        primaryColorMatcher: /#a00(\d{3})/g,
+        accentColorMatcher: /#b00(\d{3})/g,
+        warnColorMatcher: /#c00(\d{3})/g,
+        primaryContrastColorMatcher: /#ac0(\d{3})/g,
+        accentContrastColorMatcher: /#bc0(\d{3})/g,
+        warnContrastColorMatcher: /#cc0(\d{3})/g
+    };
+}
+
+/**
+ * @param isDark Whether or not the theme should be a dark theme. Defaults to `false`.
+ * @see TemplateThemeCreationOptions
+ */
+export interface ThemeCreationOptions extends Omit<
+    TemplateThemeCreationOptions,
+    "templateData" | "templateOptions"
+> {
+    isDark?: boolean;
+}
+
 
 // @dynamic
 export class ThemeGenerator {
 
-    private static readonly THEME_NAME_MATCHER = /%theme-name%/g;
-    private static readonly PRIMARY_COLOR_MATCHER = /#a00(\d{3})/g;
-    private static readonly ACCENT_COLOR_MATCHER = /#b00(\d{3})/g;
-    private static readonly WARN_COLOR_MATCHER = /#c00(\d{3})/g;
-    private static readonly PRIMARY_FONT_COLOR_MATCHER = /#ac0(\d{3})/g;
-    private static readonly ACCENT_FONT_COLOR_MATCHER = /#bc0(\d{3})/g;
-    private static readonly WARN_FONT_COLOR_MATCHER = /#cc0(\d{3})/g;
-
     /**
-     * @description Creates a new theme from the given palettes and loads it for use with the
-     * given name.
-     * @param name The name that will be used to reference the new theme.
-     * @param primaryPalette The primary color palette.
-     * @param accentPalette The accent color palette.
-     * @param warnPalette The warn color palette.
-     * @param isDark Whether or not the theme should be a dark theme. Defaults to `false`.
-     * @return The compiled theme.
+     * @description Creates a new theme from the given template theme CSS and theme palettes and loads it for use with the given name.
+     * @param options The options to use to create the new theme.
+     * @return The compiled theme CSS.
     */
-    public static create(
-        name: string,
-        primaryPalette: ThemeGenerator.Palette,
-        accentPalette: ThemeGenerator.Palette,
-        warnPalette: ThemeGenerator.Palette,
-        isDark?: boolean
-    ): string {
-        let themeData: string = this.loadThemeData(isDark);
+    public static createFromTemplate(options: TemplateThemeCreationOptions): string {
+        let themeData = options.templateData;
+        let templateOptions = Object.assign({}, ThemeTemplateOptions.defaultValues, options.templateOptions || {});
 
-        themeData = themeData.replace(this.THEME_NAME_MATCHER, name);
-        themeData = themeData.replace(this.PRIMARY_COLOR_MATCHER, (_match, $1) => primaryPalette[this.offset($1)]);
-        themeData = themeData.replace(this.ACCENT_COLOR_MATCHER, (_match, $1) => accentPalette[this.offset($1)]);
-        themeData = themeData.replace(this.WARN_COLOR_MATCHER, (_match, $1) => warnPalette[this.offset($1)]);
+        themeData = themeData.replace(templateOptions.nameMatcher, name);
+        themeData = themeData.replace(templateOptions.primaryColorMatcher, (_match, $1) => options.primaryPalette[this.offset($1)]);
+        themeData = themeData.replace(templateOptions.accentColorMatcher, (_match, $1) => options.accentPalette[this.offset($1)]);
+        themeData = themeData.replace(templateOptions.warnColorMatcher, (_match, $1) => options.warnPalette[this.offset($1)]);
 
-        themeData = themeData.replace(this.PRIMARY_FONT_COLOR_MATCHER, (_match, $1) => primaryPalette.contrast[this.offset($1)]);
-        themeData = themeData.replace(this.ACCENT_FONT_COLOR_MATCHER, (_match, $1) => accentPalette.contrast[this.offset($1)]);
-        themeData = themeData.replace(this.WARN_FONT_COLOR_MATCHER, (_match, $1) => warnPalette.contrast[this.offset($1)]);
+        themeData = themeData.replace(templateOptions.primaryContrastColorMatcher, (_match, $1) => options.primaryPalette.contrast[this.offset($1)]);
+        themeData = themeData.replace(templateOptions.accentContrastColorMatcher, (_match, $1) => options.accentPalette.contrast[this.offset($1)]);
+        themeData = themeData.replace(templateOptions.warnContrastColorMatcher, (_match, $1) => options.warnPalette.contrast[this.offset($1)]);
 
         ThemeLoader.loadCompiled(themeData);
         return themeData;
     }
 
     /**
-     * @description Creates a new theme from the given base colors and loads it for use with the
-     * given name.
-     * @param name The name that will be used to reference the new theme.
-     * @param primaryColor The color that will be used as the base color for the primary palette.
-     * @param accentColor The color that will be used as the base color for the accent palette.
-     * @param warnColor The color that will be used as the base color for the warn palette.
-     * @param isDark Whether or not the theme should be a dark theme. Defaults to `false`.
+     * @description Creates a new theme from the given theme palettes and loads it for use with the given name.
+     * @param options The options to use to create the new theme.
      * @return The compiled theme.
     */
-    public static createBasic(
-        name: string,
-        primaryColor: string,
-        accentColor: string,
-        warnColor: string,
-        isDark?: boolean
-    ): string {
-        return this.create(name,
-            this.computePalette(primaryColor),
-            this.computePalette(accentColor),
-            this.computePalette(warnColor),
-            isDark
-        );
+    public static create(options: ThemeCreationOptions): string {
+        return this.createFromTemplate({
+            templateData: this.loadStandardThemeTemplate(options.isDark),
+            ...options
+        });
     }
 
     /**
-     * @description Creates a new random theme and loads it for use with the given name.
-     * @param name The name that will be used to reference the new theme.
-     * @param isDark Whether or not the theme should be a dark theme. If not 
-     * specified, a light or dark theme will be choosen at random.
-     * @return The compiled theme.
-    */
-    public static createRandom(name: string, isDark?: boolean): string {
-        try {
-            const chroma = require("chroma-js");
-
-            return this.createBasic(name,
-                chroma.random().hex(),
-                chroma.random().hex(),
-                chroma.random().hex(),
-                isDark === undefined ? Math.random() >= 0.5 : isDark
-            );
-        } catch(_e) {
-            throw new Error("'chroma-js' is required to generate dynamic runtime themes.");
-        }
-    }
-
-    /** @description Computes a palette for the given base color. */
-    private static computePalette(baseColor: string): ThemeGenerator.Palette {
+     * @description Computes a Material color palette for the given `baseColor`.
+     * @param baseColor The color that the palette will be based off of.
+     * @requires chroma-js
+     */
+    public static createPalette(baseColor: string): ThemeGenerator.Palette {
         const basePalette = {
-            50: this.computePaletteColor(baseColor, 50),
-            100: this.computePaletteColor(baseColor, 100),
-            200: this.computePaletteColor(baseColor, 200),
-            300: this.computePaletteColor(baseColor, 300),
-            400: this.computePaletteColor(baseColor, 400),
-            500: this.computePaletteColor(baseColor, 500),
-            600: this.computePaletteColor(baseColor, 600),
-            700: this.computePaletteColor(baseColor, 700),
-            800: this.computePaletteColor(baseColor, 800),
-            900: this.computePaletteColor(baseColor, 900)
+            50: this.paletteColor(baseColor, 50),
+            100: this.paletteColor(baseColor, 100),
+            200: this.paletteColor(baseColor, 200),
+            300: this.paletteColor(baseColor, 300),
+            400: this.paletteColor(baseColor, 400),
+            500: this.paletteColor(baseColor, 500),
+            600: this.paletteColor(baseColor, 600),
+            700: this.paletteColor(baseColor, 700),
+            800: this.paletteColor(baseColor, 800),
+            900: this.paletteColor(baseColor, 900)
         };
 
         return Object.assign(basePalette, {
             contrast: {
-                50: this.computeFontColor(basePalette[50]),
-                100: this.computeFontColor(basePalette[100]),
-                200: this.computeFontColor(basePalette[200]),
-                300: this.computeFontColor(basePalette[300]),
-                400: this.computeFontColor(basePalette[400]),
-                500: this.computeFontColor(basePalette[500]),
-                600: this.computeFontColor(basePalette[600]),
-                700: this.computeFontColor(basePalette[700]),
-                800: this.computeFontColor(basePalette[800]),
-                900: this.computeFontColor(basePalette[900])
+                50: this.contrastColor(basePalette[50]),
+                100: this.contrastColor(basePalette[100]),
+                200: this.contrastColor(basePalette[200]),
+                300: this.contrastColor(basePalette[300]),
+                400: this.contrastColor(basePalette[400]),
+                500: this.contrastColor(basePalette[500]),
+                600: this.contrastColor(basePalette[600]),
+                700: this.contrastColor(basePalette[700]),
+                800: this.contrastColor(basePalette[800]),
+                900: this.contrastColor(basePalette[900])
             }
         });
     }
 
-    /** @description Computes a color for a Material font palette using `baseColor` and the given `paletteOffset`. */
-    private static computePaletteColor(baseColor: string, paletteOffset: PaletteOffset): string {
+    /**
+     * @description Computes a color for a Material theme palette using `baseColor` and the given `paletteOffset`.
+     * @param baseColor The base color.
+     * @param paletteOffset The offset in the palette.
+     * @return The new color to be used for the given palette offset.
+     * @requires chroma-js
+     */
+    public static paletteColor(baseColor: string, paletteOffset: PaletteOffset): string {
         try {
             const chroma = require("chroma-js");
 
@@ -137,12 +147,17 @@ export class ThemeGenerator {
                 return chroma(baseColor).darken((paletteOffset - 500) / 1000).hex();
             }
         } catch(_e) {
-            throw new Error("'chroma-js' is required to generate dynamic runtime themes.");
+            throw new Error("'chroma-js' is required to generate basic runtime themes.");
         }
     }
 
-    /** @description Computes an appropriate font color (either black or white) that contrasts the given color. */
-    private static computeFontColor(color: string): string {
+    /**
+     * @description Computes an appropriate contrast color (either black or white) that contrasts the given `color`.
+     * @param color The color to be contrasted.
+     * @return The contrast color.
+     * @requires chroma-js
+     */
+    public static contrastColor(color: string): string {
         try {
             const chroma = require("chroma-js");
 
@@ -156,7 +171,7 @@ export class ThemeGenerator {
         }
     }
 
-    private static loadThemeData(isDark: boolean): string {
+    private static loadStandardThemeTemplate(isDark: boolean): string {
         return isDark ? BASIC_DARK_THEME_TEMPLATE : BASIC_LIGHT_THEME_TEMPLATE;
     }
 
